cfb860baf4f66ea1d7bd82211a099f73dcf5732e
Vando dos reis <vandodosreis2001@gmail.com>
2020-10-09 00:32:10 -0300

    partes mais importantes finalizadas

--- app.js
+++ app.js
@@ -62,6 +62,7 @@ class Bd {
 				continue
 			}
 
+			despesa.id = i
 			despesas.push(despesa)
 		}
 		return despesas
@@ -100,6 +101,9 @@ class Bd {
 		return despesasFiltadas
 	}	
 
+	remover(id) {
+		localStorage.removeItem(id)
+	}
 }
 
 let bd = new Bd()
@@ -180,7 +184,7 @@ function carregaListaDespesas(despesas = new Array(), filtro = false) {
 	listaDespesas.innerHTML = ''
 
 	// percorrer o array despesas listando cada despesa de forma dinâmica
-	despesas.forEach(function(d) {
+	despesas.forEach(d => {
 		//console.log(d)
 	  // Criando a linha tr
 	  let linha = listaDespesas.insertRow()
@@ -205,6 +209,19 @@ function carregaListaDespesas(despesas = new Array(), filtro = false) {
 		linha.insertCell(2).innerHTML = d.descricao
 		linha.insertCell(3).innerHTML = d.valor
 
+		// Criar um botão para exclusão
+		let btn = document.createElement('button')
+		btn.className = 'btn btn-danger'
+		btn.innerHTML = '<i class="fas fa-times"></i>'
+		btn.id = `id_despesa_${d.id}`
+		btn.onclick = function() { 
+			let id = this.id.replace('id_despesa_', '')
+			
+			bd.remover(id)
+			
+			window.location.reload()
+		}
+		linha.insertCell(4).append(btn)
 	})
 
 }
